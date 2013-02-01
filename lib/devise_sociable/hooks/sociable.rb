d432327a6948e2a2c4ff56ac0092fdbee80742f8
Michel Benevento <mbenevento@yahoo.com>
2013-02-01 11:19:29 +0100

    hook fix

--- lib/devise_sociable/hooks/sociable.rb
+++ lib/devise_sociable/hooks/sociable.rb
@@ -2,6 +2,7 @@ Warden::Manager.after_set_user do |record, warden, opts|
   if record.class.uses_cookies
     record.update_attribute('last_request_at', Time.zone.now)
   else
+    scope = opts[:scope]
     warden.session(scope)['last_request_at'] = Time.zone.now
   end
 end
